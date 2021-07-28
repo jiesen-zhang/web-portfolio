a89fc2608ab84c693e5d0709ad3917cd0e02e0be
Jiesen_Zhang <jiesenzhang0723@gmail.com>
2021-07-28 14:27:41 -0500

    removed button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -20,11 +20,11 @@ const HeroSection = () => {
         <HeroP>
           Welcome to my website
         </HeroP>
-        <HeroBtnWrapper>
+        {/* <HeroBtnWrapper>
           <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
             Get started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
-        </HeroBtnWrapper>
+        </HeroBtnWrapper> */}
       </HeroContent>
     </HeroContainer>
   )
