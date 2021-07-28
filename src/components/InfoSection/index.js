5cf9e6fe3e0ea08df9740ee3b6ef46413301f341
Jiesen_Zhang <jiesenzhang0723@gmail.com>
2021-07-28 14:27:13 -0500

    removed buttons, added text content

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -14,16 +14,16 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headline, darkT
               <TopLine>{topLine}</TopLine>
               <Heading lightText={lightText}>{headline}</Heading>
               <Subtitle darkText={darkText}>{description}</Subtitle>
-              <BtnWrap>
+              {/* <BtnWrap>
                 <Button to="home" smooth={true} duration={500} spy={true} exact="true" offset={-80} primary={primary ? 1 : 0} dark={dark ? 1 : 0} dark2={dark2 ? 1 : 0}>{buttonLabel}</Button>
-              </BtnWrap>
+              </BtnWrap> */}
             </TextWrapper>
             </Column1>
-            <Column2>
+            {/* <Column2>
               <ImgWrap>
               <Img src={img} alt={alt}/>
               </ImgWrap>
-            </Column2>
+            </Column2> */}
           </InfoRow>
         </InfoWrapper>
       </InfoContainer>
